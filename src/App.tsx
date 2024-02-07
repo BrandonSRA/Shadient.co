4b0e6de2532cd86ac1c80cd9f8e90c7bd0e515d2
BrandonSRA <brandon33455@gmail.com>
2024-02-07 11:23:54 -0500

    feat: :sparkles: navbar was added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { CssBaseline } from "@mui/material";
+import { NavBar } from "./components/NavBar";
 import { LandingPage } from "./pages/LandingPages";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={createTheme()}>
+      <CssBaseline />
+      <NavBar />
       <LandingPage />
-    </>
+    </ThemeProvider>
   );
 }
 
